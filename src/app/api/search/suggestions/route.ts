292d56ec6911301abf9751b3b449000dfa8e5af5
saintnicolas330 <nicolaszhong@gmail.com>
2026-03-02 22:06:50 +0800

    Update route.ts

--- src/app/api/search/suggestions/route.ts
+++ src/app/api/search/suggestions/route.ts
@@ -7,6 +7,7 @@ import { getAuthInfoFromCookie } from '@/lib/auth';
 import { getAvailableApiSites, getConfig } from '@/lib/config';
 import { searchFromApi } from '@/lib/downstream';
 import { yellowWords } from '@/lib/yellow';
+export const dynamic = 'force-dynamic';
 
 export const runtime = 'nodejs';
 
